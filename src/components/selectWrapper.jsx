59e47b3467668184df570e70c21420e2faba8990
dmitrykarpenko <dmitry.karpenko@gmail.com>
2018-10-01 17:34:06 +0300

    SelectWrapper - 'react-select' added; index.js - bootstrap imported

--- src/components/selectWrapper.jsx
+++ src/components/selectWrapper.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+
+const scaryAnimals = [
+    { label: "Alligators", value: 1 },
+    { label: "Crocodiles", value: 2 },
+    { label: "Sharks", value: 3 },
+    { label: "Small crocodiles", value: 4 },
+    { label: "Smallest crocodiles", value: 5 },
+    { label: "Snakes", value: 6 },
+];
 
 class SelectWrapper extends Component {
     // state = {  }
-    render() { 
+    render() {
         return (
-            <h1>Select wrapper test</h1>
+            <div className="row">
+                <h1>Select wrapper test</h1>
+                <div className="col-md-offset-3 col-md-6">
+                    <Select options={scaryAnimals} />
+                </div>
+            </div>
         );
     }
 }
