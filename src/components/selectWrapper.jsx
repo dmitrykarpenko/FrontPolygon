935e097b9f57f9fe843cf632592f301122a6bc61
dmitrykarpenko <dmitry.karpenko@gmail.com>
2018-10-01 17:14:24 +0300

    SelectWrapper - initial; react-select package installed

---
+++ src/components/selectWrapper.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SelectWrapper extends Component {
+    // state = {  }
+    render() { 
+        return (
+            <h1>Select wrapper test</h1>
+        );
+    }
+}
+ 
+export default SelectWrapper;
